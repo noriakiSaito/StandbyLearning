8e8f30792ff6b02c49e240aba1c0b3fb9e6fd0ba
git config --global user.name "kirinoa1 <git config --global user.email "Kirinoa1@co.jp>
2020-07-09 18:39:29 +0900

    2020/7/8～2020/7/9 の成果物

---
+++ SpringBoot2 プログラミング入門/Chapter99_復習/01_SpringBootとPostgreSQLで作ったAPI/demo/src/main/java/com/example/demo/Client.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String mail;
+	@Column
+	private String memo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+}
